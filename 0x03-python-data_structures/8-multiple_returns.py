b36f4812370fed85bed2a4f9bcb806700c69e628
TheGodwill <101563346+TheGodwill@users.noreply.github.com>
2022-07-03 21:20:42 +0000

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,4 @@
-#!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is "":
-        tup = (len(sentence), None)
-        return tup
-    else:
-        tup = (len(sentence), sentence[0])
-        return tup
+    if len(sentence) == 0:
+        return (0, None)
+    return (len(sentence), sentence[0])
