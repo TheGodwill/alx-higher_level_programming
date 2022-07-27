2f5d64013e2e73b29d23bb7ec1e63afd06b7c939
TheGodwill <sielle.sorho@gmail.com>
2022-07-26 17:51:47 -0700

    Python - Everything is object Fixed !

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(list_print=[]):
-    list_print += ["Holberton"]
-    return (", ".join(list_print))
+def magic_string(str=[]):
+    str.append("Holberton")
+    return ", ".join(str)
